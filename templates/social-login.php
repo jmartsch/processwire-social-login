7dc487ca052bcde7affcd67247538a90311a9387
Jens Martsch <jmartsch@gmail.com>
2016-12-14 19:33:20 +0100

    added minimum-stability Added compability to ProcessWire Namespace

--- templates/social-login.php
+++ templates/social-login.php
@@ -1,81 +1,77 @@
-<?php
-/**
- * Template file for SocialLogin
- *
- * This should be copied to /site/templates/ folder if installation script cannot do it
- * 
- * @see https://processwire.com/talk/topic/107-custom-login/
- */
+<?php namespace ProcessWire;
 
-$SocialLogin = $this->modules->get( "SocialLogin" );
-if ( ! $SocialLogin ) {
-    throw new Wire404Exception( 'SocialLogin module is not installed' );
-}
+    /**
+     * Template file for SocialLogin
+     *
+     * This should be copied to /site/templates/ folder if installation script cannot do it
+     *
+     * @see https://processwire.com/talk/topic/107-custom-login/
+     */
 
-if ( isset( $_REQUEST['hybridauth'] ) ) :
+    $SocialLogin = $this->modules->get("SocialLogin");
+    if (!$SocialLogin) {
+        throw new Wire404Exception('SocialLogin module is not installed');
+    }
 
-    // Process the request
-    $SocialLogin->HybridAuthProcess();
+    if (isset($_REQUEST['hybridauth'])) :
 
-else:
+        // Process the request
+        $SocialLogin->HybridAuthProcess();
 
-ob_start();
-?>
+    else:
 
-<div class="content">
+        ob_start();
+        ?>
 
-    <?php
-        if ( $user->isLoggedin() ) {
-            echo '<h1>'. __( 'Welcome' ) . ' ' . $user->name .'</h1>';
+        <div class="content">
 
-            echo "<p>Da questa pagina puoi gestire il tuo profilo e fare tante altre cose</p>";
+            <?php
+                if ($user->isLoggedin()) {
+                    echo '<h1>' . __('Welcome') . ' ' . $user->name . '</h1>';
 
-            echo "<br><hr><br>";
 
-            $SocialLogin->showProfile();
+                    $SocialLogin->showProfile();
 
-            ?><a class='action' href='<?php echo $config->urls->admin; ?>login/logout/'><?php echo __( 'Logout' ); ?></a><?php
+                    ?>
+                    <a class='action' href='<?php echo $config->urls->admin; ?>login/logout/'><?php echo __('Logout'); ?></a><?php
 
-        }
-        else {
-            echo '<h1>'.$title.'</h1>';
+                } else {
+                    echo '<h1>' . $title . '</h1>';
 
-            $SocialLogin->showLogin();
-        }
-    ?>
+                    $SocialLogin->showLogin();
+                }
+            ?>
 
-</div>
+        </div>
 
-<?php
+        <?php
 
 // User login through email/pass
 // 
-if ( $input->post->slogin_submit && $input->post->slogin_email ) {
-    $email = $sanitizer->email( $input->post->slogin_email );
-    $emailUser = $users->get( "email=$email" );
-    if ( $emailUser->id ) {
-        $user = $session->login( $emailUser->name, $input->post->slogin_pass );
-        if ( $user ) {
-            $session->redirect( $page->path );
+        if ($input->post->slogin_submit && $input->post->slogin_email) {
+            $email = $sanitizer->email($input->post->slogin_email);
+            $emailUser = $users->get("email=$email");
+            if ($emailUser->id) {
+                $user = $session->login($emailUser->name, $input->post->slogin_pass);
+                if ($user) {
+                    $session->redirect($page->path);
+                } else {
+                    echo __("Login failed!");
+                }
+            } else {
+                echo __("Unrecognized email address");
+            }
         }
-        else {
-            echo __( "Login failed!" );
+
+        if (isset($_REQUEST["provider"])) {
+            try {
+                echo $SocialLogin->execute($_REQUEST["provider"]);
+            } catch (Exception $e) {
+                $SocialLogin->showError(sprintf(__('An error occours: %s'), $e->getMessage()));
+            }
         }
-    }
-    else {
-        echo __( "Unrecognized email address" );
-    }
-}
 
-if ( isset( $_REQUEST["provider"] ) ) {
-    try {
-        echo $SocialLogin->execute( $_REQUEST["provider"] );
-    }
-    catch ( Exception $e ) {
-        $SocialLogin->showError( sprintf( __( 'An error occours: %s' ), $e->getMessage() ) );
-    }
-}
+        $content = ob_get_clean();
 
-$content = ob_get_clean();
-
-endif;
+        echo $content;
+    endif;
